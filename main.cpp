a197d2faa31e698ad8d5040a9cf5d4a3a9bb7185
orifha003 <orifha03mulovhedzi@gmail.com>
2026-09-20 01:43:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() {
+
+    //student information
     std::string name;
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
-    std::cout << "Hello, " << name << "!" << std::endl;
-    return 0;
-}
+    int studentNumber;
+    double mark;
+
+    //display tittle 
+    cout <<"--------------------" << std::endl;
+    cout << "STUDENT MANAGEMENT SYSTEM" << std::endl;
+    cout << "-------------------" << std::endl;
+
+    //get student name from user 
+    cout << "Enter your name: ";
+    getline(cin, name);
+
+    //get student number from user
+    cout << "Enter your student number: ";
+    cin >> studentNumber;
+    
+    //get student mark from user
+    cout << "Enter your mark: ";
+    cin >> mark;
+    
